19d6d5dc8afbab462a8a699a7747e24b0ea7b023
Khaled <khaled.shbair12@gmail.com>
2023-09-05 20:27:18 +0300

    create manager_sizes.dart contain all sizes of app

---
+++ lib/core/resources/manager_sizes.dart
@@ -0,0 +1,21 @@
+import '/config/all_imports.dart';
+
+class ManagerHeight {
+  static double h20 = SizeUtil.setHeight(20);
+}
+
+class ManagerWidth {
+  static double w20 = SizeUtil.setWidth(20);
+}
+
+class ManagerRadius {
+  static double r20 = SizeUtil.setRadius(20);
+}
+
+class ManagerIconSize {
+  static const double s24 = 24;
+}
+
+class ManagerOpacity {
+  static double op0_2 = 0.2;
+}
