a8895e373762f91cd00b8edf3f2e34c3e7ec6864
Khaled <khaled.shbair12@gmail.com>
2023-09-05 20:32:50 +0300

    create AppConstants contain all constants of app

---
+++ lib/config/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const double designSizeWidth = 375;
+  static const double designSizeHeight = 812;
+  static const int durationSplashScreen = 3;
+  static const int durationOnBoarding = 1;
+  static const double elevationAppBar = 0;
+}
