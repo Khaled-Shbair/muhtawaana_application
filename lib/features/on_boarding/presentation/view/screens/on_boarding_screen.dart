0024ae6749943a3306462801384d20c779eb4bcc
Khaled <khaled.shbair12@gmail.com>
2023-09-06 01:43:26 +0300

    add willPopScope widget

--- lib/features/on_boarding/presentation/view/screens/on_boarding_screen.dart
+++ lib/features/on_boarding/presentation/view/screens/on_boarding_screen.dart
@@ -5,113 +5,115 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<OnBoardingController>(
-      builder: (controller) {
-        controller.goToNextPage();
-        return Scaffold(
-          backgroundColor: ManagerColors.whiteColor,
-          resizeToAvoidBottomInset: true,
-          appBar: AppBar(
-            leading: Visibility(
-              visible: controller.isNotFirstPage(),
-              child: IconButton(
-                onPressed: controller.previousPage,
-                icon: const Icon(
-                  Icons.arrow_back,
-                  color: ManagerColors.c2,
+    return willPopScope(
+      child: GetBuilder<OnBoardingController>(
+        builder: (controller) {
+          controller.goToNextPage();
+          return Scaffold(
+            backgroundColor: ManagerColors.whiteColor,
+            resizeToAvoidBottomInset: true,
+            appBar: AppBar(
+              leading: Visibility(
+                visible: controller.isNotFirstPage(),
+                child: IconButton(
+                  onPressed: controller.previousPage,
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: ManagerColors.c2,
+                  ),
                 ),
               ),
             ),
-          ),
-          body: Padding(
-            padding: EdgeInsetsDirectional.only(
-              start: ManagerWidth.w16,
-              end: ManagerWidth.w16,
-              top: ManagerHeight.h16,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: PageView(
-                    controller: controller.pageController,
-                    onPageChanged: (index) => controller.setCurrentPage(index),
+            body: Padding(
+              padding: EdgeInsetsDirectional.only(
+                start: ManagerWidth.w16,
+                end: ManagerWidth.w16,
+                top: ManagerHeight.h16,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: PageView(
+                      controller: controller.pageController,
+                      onPageChanged: (index) => controller.setCurrentPage(index),
+                      children: [
+                        ...List.generate(
+                          controller.pageViewItems.length,
+                          (index) => PageViewItem(
+                            title: controller.pageViewItems[index].title,
+                            subTitle: controller.pageViewItems[index].subTitle,
+                            image: controller.pageViewItems[index].image,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Stack(
                     children: [
-                      ...List.generate(
-                        controller.pageViewItems.length,
-                        (index) => PageViewItem(
-                          title: controller.pageViewItems[index].title,
-                          subTitle: controller.pageViewItems[index].subTitle,
-                          image: controller.pageViewItems[index].image,
+                      SizedBox(
+                        height: ManagerHeight.h50,
+                        width: ManagerWidth.w50,
+                        child: CircularProgressIndicator(
+                          color: ManagerColors.primaryColor,
+                          backgroundColor: ManagerColors.c3,
+                          strokeWidth: 10,
+                          value: controller.valueCircularIndicator,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: ManagerRadius.r25,
+                        backgroundColor: ManagerColors.c1,
+                        child: const Icon(
+                          Icons.arrow_forward,
+                          color: ManagerColors.c2,
                         ),
                       ),
                     ],
                   ),
-                ),
-                Stack(
-                  children: [
-                    SizedBox(
-                      height: ManagerHeight.h50,
-                      width: ManagerWidth.w50,
-                      child: CircularProgressIndicator(
-                        color: ManagerColors.primaryColor,
-                        backgroundColor: ManagerColors.c3,
-                        strokeWidth: 10,
-                        value: controller.valueCircularIndicator,
-                      ),
+                  Visibility(
+                    visible: controller.isNotLastPage(),
+                    replacement: MainButton(
+                      onPressed: controller.goToAuthScreen,
+                      text: ManagerStrings.getStarted,
+                      start: ManagerWidth.w92,
+                      end: ManagerWidth.w92,
+                      top: ManagerHeight.h16,
+                      bottom: ManagerHeight.h4,
                     ),
-                    CircleAvatar(
-                      radius: ManagerRadius.r25,
-                      backgroundColor: ManagerColors.c1,
-                      child: const Icon(
-                        Icons.arrow_forward,
-                        color: ManagerColors.c2,
-                      ),
+                    child: MainButton(
+                      onPressed: controller.nextPage,
+                      text: ManagerStrings.continueWord,
+                      start: ManagerWidth.w92,
+                      end: ManagerWidth.w92,
+                      top: ManagerHeight.h16,
+                      bottom: ManagerHeight.h4,
                     ),
-                  ],
-                ),
-                Visibility(
-                  visible: controller.isNotLastPage(),
-                  replacement: MainButton(
-                    onPressed: controller.goToAuthScreen,
-                    text: ManagerStrings.getStarted,
-                    start: ManagerWidth.w92,
-                    end: ManagerWidth.w92,
-                    top: ManagerHeight.h16,
-                    bottom: ManagerHeight.h4,
                   ),
-                  child: MainButton(
-                    onPressed: controller.nextPage,
-                    text: ManagerStrings.continueWord,
-                    start: ManagerWidth.w92,
-                    end: ManagerWidth.w92,
-                    top: ManagerHeight.h16,
-                    bottom: ManagerHeight.h4,
+                  Visibility(
+                    visible: controller.isNotLastPage(),
+                    maintainAnimation: true,
+                    maintainInteractivity: true,
+                    maintainSemantics: true,
+                    maintainSize: true,
+                    maintainState: true,
+                    child: MainButton(
+                      onPressed: controller.skipPage,
+                      text: ManagerStrings.skip,
+                      color: ManagerColors.transparentColor,
+                      start: ManagerWidth.w92,
+                      end: ManagerWidth.w92,
+                      bottom: ManagerHeight.h29,
+                      colorText: ManagerColors.c4,
+                    ),
                   ),
-                ),
-                Visibility(
-                  visible: controller.isNotLastPage(),
-                  maintainAnimation: true,
-                  maintainInteractivity: true,
-                  maintainSemantics: true,
-                  maintainSize: true,
-                  maintainState: true,
-                  child: MainButton(
-                    onPressed: controller.skipPage,
-                    text: ManagerStrings.skip,
-                    color: ManagerColors.transparentColor,
-                    start: ManagerWidth.w92,
-                    end: ManagerWidth.w92,
-                    bottom: ManagerHeight.h29,
-                    colorText: ManagerColors.c4,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
