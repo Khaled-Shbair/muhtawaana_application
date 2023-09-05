a38eacd05d42d7439fa73075e200f6240115d42b
Khaled <khaled.shbair12@gmail.com>
2023-09-05 23:26:24 +0300

    add new in manager_fonts.dart

--- lib/core/resources/manager_fonts.dart
+++ lib/core/resources/manager_fonts.dart
@@ -1,7 +1,10 @@
 import '/config/all_imports.dart';
 
 class ManagerFontSize {
+  static double s14 = SizeUtil.setFontSize(14);
+  static double s17 = SizeUtil.setFontSize(17);
   static double s18 = SizeUtil.setFontSize(18);
+  static double s20 = SizeUtil.setFontSize(20);
 }
 
 class ManagerFontWeight {
@@ -13,5 +16,6 @@ class ManagerFontWeight {
 }
 
 class ManagerFontFamily {
-  // static const String  = '';
+   static const String  roboto= 'Roboto';
+   static const String  inter= 'Inter';
 }
