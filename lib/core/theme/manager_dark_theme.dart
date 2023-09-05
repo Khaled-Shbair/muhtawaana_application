24642e744ee57886ee734b942d8b9e2112b8340b
Khaled <khaled.shbair12@gmail.com>
2023-09-05 20:28:55 +0300

    create managerDarkThemeData

---
+++ lib/core/theme/manager_dark_theme.dart
@@ -0,0 +1,23 @@
+import '/config/all_imports.dart';
+
+ThemeData managerDarkThemeData() {
+  return ThemeData.dark().copyWith(
+    useMaterial3: true,
+    primaryColor: ManagerColors.primaryColor,
+    // primaryColorDark: ManagerColors.primaryColorDark,
+    // scaffoldBackgroundColor: ManagerColors.scaffoldBackgroundColorDark,
+    textTheme: ManagerTextThemeDark(),
+    appBarTheme: const AppBarTheme(
+      // backgroundColor: ManagerColors.appBarBackgroundColorDark,
+      elevation: AppConstants.elevationAppBar,
+      centerTitle: true,
+      //titleTextStyle: boldTextStyle(
+      //  // color: ManagerColors.black,
+      //  fontSize: ManagerFontSize.s18,
+      //  // fontFamily: ManagerFontFamily.poppins,
+      //),
+    ),
+    // this for disabled button color
+    // disabledColor: ManagerColors.disabledColorDark,
+  );
+}
