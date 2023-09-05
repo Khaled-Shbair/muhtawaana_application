e3e73b97fdeb9089f6904a66f77d44d7f035194d
Khaled <khaled.shbair12@gmail.com>
2023-09-05 23:26:06 +0300

    add new colors

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -2,5 +2,12 @@ import '/config/all_imports.dart';
 
 class ManagerColors {
   static const Color primaryColor = Color(0XFFFF9B5A);
+  static const Color c1 = Color(0XFFD9D9D9);
+  static const Color c2 = Color(0XFF200E32);
+  static const Color c3 = Color(0XFFCFD8DC);
+  static const Color c4 = Color(0XFF979797);
   static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.black;
+  static const Color transparentColor = Colors.transparent;
+  static const Color textGeryColor = Color(0XFF6A6A6A);
 }
