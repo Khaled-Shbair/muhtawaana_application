8d8d7e4b3acce17fe44a3fa1a057922a4d46231b
Khaled <khaled.shbair12@gmail.com>
2023-09-05 21:27:01 +0300

    add images splash screen

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -2,12 +2,12 @@ const String _imagePath = 'assets/images/';
 // const String _jsonPath = 'assets/json/';
 
 class ManagerAssets {
-  static const String logo = '${_imagePath}logo.svg';
+  static const String logo = '${_imagePath}logo.png';
   static const String imageOnBoardingOne = '${_imagePath}on_boarding_one.svg';
   static const String imageOnBoardingTwo = '${_imagePath}on_boarding_two.svg';
   static const String imageOnBoardingThree =
       '${_imagePath}on_boarding_three.svg';
-  static const String backgroundSplash = '${_imagePath}background_splash.svg';
+  static const String backgroundSplash = '${_imagePath}background_splash.png';
 }
 
 class ManagerJson {}
