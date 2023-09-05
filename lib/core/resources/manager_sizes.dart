0accea5a84cf42d4c636e7c0870f64789782311d
Khaled <khaled.shbair12@gmail.com>
2023-09-05 23:35:20 +0300

    add new sizes

--- lib/core/resources/manager_sizes.dart
+++ lib/core/resources/manager_sizes.dart
@@ -1,19 +1,34 @@
 import '/config/all_imports.dart';
 
 class ManagerHeight {
+  static double h4 = SizeUtil.setHeight(4);
+  static double h16 = SizeUtil.setHeight(16);
   static double h20 = SizeUtil.setHeight(20);
+  static double h29 = SizeUtil.setHeight(29);
+  static double h48 = SizeUtil.setHeight(48);
+  static double h50 = SizeUtil.setHeight(50);
+  static double h76 = SizeUtil.setHeight(76);
   static double h77 = SizeUtil.setHeight(77);
   static double h120 = SizeUtil.setHeight(120);
 }
 
 class ManagerWidth {
+  static double w16 = SizeUtil.setWidth(16);
   static double w20 = SizeUtil.setWidth(20);
+  static double w36 = SizeUtil.setWidth(36);
+  static double w50 = SizeUtil.setHeight(50);
+  static double w92= SizeUtil.setHeight(92);
+
   static double w102 = SizeUtil.setWidth(102);
   static double w120 = SizeUtil.setWidth(120);
+  static double w261 = SizeUtil.setWidth(261);
 }
 
 class ManagerRadius {
+  static double r5 = SizeUtil.setRadius(5);
   static double r20 = SizeUtil.setRadius(20);
+  static double r25 = SizeUtil.setRadius(25);
+  static double r100 = SizeUtil.setRadius(100);
 }
 
 class ManagerIconSize {
