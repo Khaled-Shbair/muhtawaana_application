164109472df5ff615c4983f27b2377001fa1fc0c
Khaled <khaled.shbair12@gmail.com>
2023-09-06 21:43:29 +0300

    add new sizes

--- lib/core/resources/manager_sizes.dart
+++ lib/core/resources/manager_sizes.dart
@@ -2,27 +2,36 @@ import '/config/all_imports.dart';
 
 class ManagerHeight {
   static double h4 = SizeUtil.setHeight(4);
+  static double h14 = SizeUtil.setHeight(14);
   static double h16 = SizeUtil.setHeight(16);
   static double h20 = SizeUtil.setHeight(20);
   static double h29 = SizeUtil.setHeight(29);
+  static double h38 = SizeUtil.setHeight(38);
   static double h48 = SizeUtil.setHeight(48);
   static double h50 = SizeUtil.setHeight(50);
   static double h60 = SizeUtil.setHeight(60);
   static double h76 = SizeUtil.setHeight(76);
   static double h77 = SizeUtil.setHeight(77);
+  static double h80 = SizeUtil.setHeight(80);
   static double h120 = SizeUtil.setHeight(120);
+  static double h324 = SizeUtil.setHeight(324);
+  static double h355 = SizeUtil.setHeight(355);
 }
 
 class ManagerWidth {
+  static double w8 = SizeUtil.setWidth(8);
+  static double w10 = SizeUtil.setWidth(10);
   static double w16 = SizeUtil.setWidth(16);
   static double w20 = SizeUtil.setWidth(20);
   static double w36 = SizeUtil.setWidth(36);
+  static double w40 = SizeUtil.setHeight(40);
   static double w50 = SizeUtil.setHeight(50);
+  static double w74 = SizeUtil.setHeight(74);
   static double w92 = SizeUtil.setHeight(92);
-
   static double w102 = SizeUtil.setWidth(102);
   static double w120 = SizeUtil.setWidth(120);
   static double w261 = SizeUtil.setWidth(261);
+  static double w305 = SizeUtil.setWidth(305);
 }
 
 class ManagerRadius {
