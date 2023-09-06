9b9f81973358d0a8f110793886f81a686fd670cf
Khaled <khaled.shbair12@gmail.com>
2023-09-06 21:42:05 +0300

    add new auth images

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -3,15 +3,23 @@ const String _imagePath = 'assets/images/';
 
 class ManagerAssets {
   static const String logo = '${_imagePath}logo.png';
+
+  //Splash images
   static const String backgroundSplash =
       '${_imagePath}splash/background_splash.png';
 
+  //OnBoarding images
   static const String imageOnBoardingOne =
       '${_imagePath}on_boarding/on_boarding_one.svg';
   static const String imageOnBoardingTwo =
       '${_imagePath}on_boarding/on_boarding_two.svg';
   static const String imageOnBoardingThree =
       '${_imagePath}on_boarding/on_boarding_three.svg';
+
+  //Auth images
+  static const String imageAuthScreen =
+      '${_imagePath}auth/image_auth_screen.svg';
+  static const String topLeftCorner = '${_imagePath}auth/top_left_corner.svg';
 }
 
 class ManagerJson {}
