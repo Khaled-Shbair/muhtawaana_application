571cc9d17c7b30243f55acd30d292e7817b69e05
Khaled <khaled.shbair12@gmail.com>
2023-09-07 20:31:36 +0300

    create DataSignUpMapper

---
+++ lib/features/auth/data/mapper/data_sign_up_mapper.dart
@@ -0,0 +1,16 @@
+import '/config/all_imports.dart';
+
+extension DataSignUpMapper on DataSignUpResponse {
+  toDomain() {
+    return DataSignUpModel(
+      id: id.onNull(),
+      name: name.onNull(),
+      image: image.onNull(),
+      token: token.onNull(),
+      email: email.onNull(),
+      phone: phone.onNull(),
+      credit: credit.onNull(),
+      points: points.onNull(),
+    );
+  }
+}
