1a2a04b9ec209edb873896c1739f92c34a818beb
Khaled <khaled.shbair12@gmail.com>
2023-09-07 20:25:31 +0300

    create RequestsApi contain all path of api request

---
+++ lib/config/constants/requests_api.dart
@@ -0,0 +1,7 @@
+class RequestsApi {
+  static const String baseUrl = 'https://student.valuxapps.com/api/';
+  static const String login = '${baseUrl}login';
+  static const String register = '${baseUrl}register';
+  static const String changePassword = '${baseUrl}change-password';
+  static const String logout = '${baseUrl}logout';
+}
