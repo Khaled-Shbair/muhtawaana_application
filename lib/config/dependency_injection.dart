8945fac5df47e26fe7f7db6a784e32a7859f9b7a
Khaled <khaled.shbair12@gmail.com>
2023-09-07 21:00:53 +0300

    add news on dependency_injection.dart

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -30,17 +30,76 @@ initLogin() {
   _disposeSplash();
   _disposeOnBoarding();
   _disposeSignUp();
+  _disposeChangePassword();
+  if (!GetIt.I.isRegistered<RemoteLoginDataSource>()) {
+    instance.registerLazySingleton<RemoteLoginDataSource>(
+      () => RemoteLoginDataSourceImplementation(instance<AppApi>()),
+    );
+  }
+  if (!GetIt.I.isRegistered<LoginRepository>()) {
+    instance.registerLazySingleton<LoginRepository>(
+      () => LoginRepositoryImplementation(
+        instance<NetworkInfo>(),
+        instance<RemoteLoginDataSource>(),
+      ),
+    );
+  }
+  if (!GetIt.I.isRegistered<LoginUseCase>()) {
+    instance.registerLazySingleton<LoginUseCase>(
+      () => LoginUseCase(instance<LoginRepository>()),
+    );
+  }
   Get.put<LoginController>(LoginController());
 }
 
-_disposeLogin() => Get.delete<LoginController>();
+_disposeLogin() async {
+  if (GetIt.I.isRegistered<RemoteLoginDataSource>()) {
+    await instance.unregister<RemoteLoginDataSource>();
+  }
+  if (GetIt.I.isRegistered<LoginRepository>()) {
+    await instance.unregister<LoginRepository>();
+  }
+  if (GetIt.I.isRegistered<LoginUseCase>()) {
+    await instance.unregister<LoginUseCase>();
+  }
+  await Get.delete<LoginController>();
+}
 
 initSignUp() {
   _disposeLogin();
+  if (!GetIt.I.isRegistered<RemoteSignUpDataSource>()) {
+    instance.registerLazySingleton<RemoteSignUpDataSource>(
+      () => RemoteSignUpDataSourceImplementation(instance<AppApi>()),
+    );
+  }
+  if (!GetIt.I.isRegistered<SignUpRepository>()) {
+    instance.registerLazySingleton<SignUpRepository>(
+      () => SignUpRepositoryImplementation(
+        instance<NetworkInfo>(),
+        instance<RemoteSignUpDataSource>(),
+      ),
+    );
+  }
+  if (!GetIt.I.isRegistered<SignUpUseCase>()) {
+    instance.registerFactory<SignUpUseCase>(
+      () => SignUpUseCase(instance<SignUpRepository>()),
+    );
+  }
   Get.put<SignUpController>(SignUpController());
 }
 
-_disposeSignUp() => Get.delete<SignUpController>();
+_disposeSignUp() async {
+  if (GetIt.I.isRegistered<RemoteSignUpDataSource>()) {
+    await instance.unregister<RemoteSignUpDataSource>();
+  }
+  if (GetIt.I.isRegistered<SignUpRepository>()) {
+    await instance.unregister<SignUpRepository>();
+  }
+  if (GetIt.I.isRegistered<SignUpUseCase>()) {
+    await instance.unregister<SignUpUseCase>();
+  }
+  await Get.delete<SignUpController>();
+}
 
 initChangePassword() {
   Get.put<ChangePasswordController>(ChangePasswordController());
