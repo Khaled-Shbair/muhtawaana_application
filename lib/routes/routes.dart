5788f4da5a931da986662ae83bd3d96f1ace32c6
Khaled <khaled.shbair12@gmail.com>
2023-09-07 11:28:47 +0300

    add new routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,4 +5,5 @@ class Routes {
   static const String signUpScreen = '/sign_up_screen';
   static const String authScreen = '/auth_screen';
   static const String mainScreen = '/main_screen';
+  static const String forgetPasswordScreen = '/forget_password_screen';
 }
