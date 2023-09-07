6ca2125c499ba65abf4adb08b4cf34eced091fd7
Khaled <khaled.shbair12@gmail.com>
2023-09-07 20:23:29 +0300

    create SignUpRequest

---
+++ lib/features/auth/data/request/sign_up_requset.dart
@@ -0,0 +1,13 @@
+class SignUpRequest {
+  String? email;
+  String? password;
+  String? phone;
+  String? name;
+
+  SignUpRequest({
+    this.email,
+    this.password,
+    this.phone,
+    this.name,
+  });
+}
