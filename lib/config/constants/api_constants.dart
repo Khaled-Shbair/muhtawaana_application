46f5b4178d3ef8c9430e8033af34b8df6069c3be
Khaled <khaled.shbair12@gmail.com>
2023-09-07 20:24:16 +0300

    generate ApiConstants

---
+++ lib/config/constants/api_constants.dart
@@ -0,0 +1,29 @@
+class ApiConstants {
+  static const int receiveTimeout = 60;
+  static const int sendTimeout = 60;
+
+  // Api header key
+  static const String bearer = 'Bearer '; // Type of token
+  static const String lang = 'lang';
+  static const String authorization = 'Authorization';
+  static const String contentType = 'Content-Type';
+
+  // Api keys
+  static const String token = 'token';
+  static const String status = 'status';
+  static const String message = 'message';
+  static const String password = 'password';
+  static const String email = 'email';
+  static const String phone = 'phone';
+  static const String id = 'id';
+
+  static const String data = 'data';
+  static const String name = 'name';
+  static const String image = 'image';
+  static const String points = 'points';
+  static const String credit = 'credit';
+
+  static const String errors = 'errors';
+  static const String badRequest = 'bad request';
+  static const String error = 'error';
+}
