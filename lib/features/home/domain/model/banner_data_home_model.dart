20346ef96fffccb0f9e21a54063db00cd0c816e4
Khaled <khaled.shbair12@gmail.com>
2023-09-09 00:19:55 +0300

    create BannerDataHomeModel

---
+++ lib/features/home/domain/model/banner_data_home_model.dart
@@ -0,0 +1,13 @@
+class BannerDataHomeModel {
+  int id;
+  String image;
+  dynamic category;
+  dynamic product;
+
+  BannerDataHomeModel({
+    required this.category,
+    required this.image,
+    required this.id,
+    required this.product,
+  });
+}
