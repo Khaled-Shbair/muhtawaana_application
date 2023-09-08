1b312ea14aef7d098706f4a00aa621559b88ebc0
Khaled <khaled.shbair12@gmail.com>
2023-09-09 00:20:17 +0300

    create HomeModel

---
+++ lib/features/home/domain/model/home_model.dart
@@ -0,0 +1,13 @@
+import '/config/all_imports.dart';
+
+class HomeModel {
+  bool status;
+  String message;
+  DataHomeModel data;
+
+  HomeModel({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+}
