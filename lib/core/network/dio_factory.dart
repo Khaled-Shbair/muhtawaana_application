b6a6d8e75cf4c6aa784c84cf5dc0653067aefc66
Khaled <khaled.shbair12@gmail.com>
2023-09-08 13:33:09 +0300

    edit type of token

--- lib/core/network/dio_factory.dart
+++ lib/core/network/dio_factory.dart
@@ -12,16 +12,14 @@ class DioFactory {
       receiveTimeout: const Duration(seconds: ApiConstants.receiveTimeout),
       sendTimeout: const Duration(seconds: ApiConstants.sendTimeout),
       headers: {
-        ApiConstants.authorization:
-            '${ApiConstants.bearer}${_sharedPreferences.getToken}',
+        ApiConstants.authorization: _sharedPreferences.getToken,
         ApiConstants.lang: _sharedPreferences.getLanguage,
         ApiConstants.contentType: 'application/json',
       },
     );
     InterceptorsWrapper interceptor = InterceptorsWrapper(
       onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
-        options.headers[ApiConstants.authorization] =
-            '${ApiConstants.bearer}${_sharedPreferences.getToken}';
+        options.headers[ApiConstants.authorization] = _sharedPreferences.getToken;
         return handler.next(options);
       },
     );
