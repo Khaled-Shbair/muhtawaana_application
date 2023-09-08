1cde42f627062f4a3f627e10612454ebbc3dd1d5
Khaled <khaled.shbair12@gmail.com>
2023-09-08 15:22:28 +0300

    edit error_handler.dart

--- lib/core/error_handler/error_handler.dart
+++ lib/core/error_handler/error_handler.dart
@@ -1,12 +1,5 @@
 import '/config/all_imports.dart';
 
-/*
-   * To avoid any error that may occur in the application
-      1- If there is an error coming from DioException (Api), return this error.
-         and if there is not statusCode coming from DioException (Api),
-         return 400.
-      2- else return my error (code: 400, message: 'bad request')
-*/
 class ErrorHandler implements Exception {
   late Failure failure;
 
