2898aef35e19a1c40200d07dfc9bd9dfe93e204d
Khaled <khaled.shbair12@gmail.com>
2023-09-08 15:49:47 +0300

    complete SignUpController

--- lib/features/auth/presentation/controller/sign_up_controller.dart
+++ lib/features/auth/presentation/controller/sign_up_controller.dart
@@ -1,15 +1,17 @@
 import '/config/all_imports.dart';
 
-class SignUpController extends GetxController {
+class SignUpController extends GetxController
+    with ShowLoadingDialog, ShowSnackBar {
   late TextEditingController email;
   late TextEditingController password;
   late TextEditingController confirmPassword;
   late TextEditingController phone;
   late TextEditingController name;
+  final SignUpUseCase _useCase = instance<SignUpUseCase>();
 
   var formKey = GlobalKey<FormState>();
-  bool obscureTextPassword = false;
-  bool obscureTextConfirmPassword = false;
+  bool obscureTextPassword = true;
+  bool obscureTextConfirmPassword = true;
   bool policyPrivacy = true;
 
   @override
@@ -37,15 +39,57 @@ class SignUpController extends GetxController {
     update();
   }
 
-  String? validatorEmail(String? email) {}
+  void performSingUp(context) {
+    if (_checkData()) {
+      if (phone.text.length == 10) {
+        if (password.text == confirmPassword.text) {
+          if (policyPrivacy) {
+            _singUp(context);
+          } else {
+            showSnackBar(ManagerStrings.pleaseAcceptTermsAndConditions, true);
+          }
+        } else {
+          showSnackBar(ManagerStrings.passwordsDoNotMatch, true);
+        }
+      } else {
+        showSnackBar(ManagerStrings.phoneNumberMustBeNumbers, true);
+      }
+    } else {
+      showSnackBar(ManagerStrings.pleaseEnterRequiredData, true);
+    }
+  }
 
-  String? validatorName(String? name) {}
+  bool _checkData() {
+    if (email.text.isNotEmpty &&
+        name.text.isNotEmpty &&
+        phone.text.isNotEmpty &&
+        password.text.isNotEmpty &&
+        confirmPassword.text.isNotEmpty) {
+      return true;
+    }
+    return false;
+  }
 
-  String? validatorPassword(String? password) {}
-
-  String? validatorConfirmPassword(String? confirmPassword) {}
-
-  String? validatorPhone(String? phone) {}
-
-  void performSingUp() {}
+  Future<void> _singUp(context) async {
+    showLoadingDialog(context);
+    (await _useCase.execute(
+      SignUpBaseUseCaseInput(
+        name: name.text.toString(),
+        phone: phone.text.toString(),
+        email: email.text.toString(),
+        password: password.text.toString(),
+      ),
+    ))
+        .fold(
+      (l) {
+        Get.back();
+        showSnackBar(l.message, true);
+      },
+      (r) async {
+        Get.back();
+        showSnackBar(r.message);
+        Get.offAllNamed(Routes.loginScreen);
+      },
+    );
+  }
 }
