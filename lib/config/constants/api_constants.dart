04ceed4427b5c9066eb2363d851ac655aae5caed
Khaled <khaled.shbair12@gmail.com>
2023-09-08 23:12:47 +0300

    add new keys api

--- lib/config/constants/api_constants.dart
+++ lib/config/constants/api_constants.dart
@@ -3,7 +3,6 @@ class ApiConstants {
   static const int sendTimeout = 60;
 
   // Api header key
-  static const String bearer = 'Bearer '; // Type of token
   static const String lang = 'lang';
   static const String authorization = 'Authorization';
   static const String contentType = 'Content-Type';
@@ -16,12 +15,23 @@ class ApiConstants {
   static const String email = 'email';
   static const String phone = 'phone';
   static const String id = 'id';
+  static const String price = 'price';
+  static const String oldPrice = 'old_price';
+  static const String discount = 'discount';
+  static const String description = 'description';
+  static const String inFavorites = 'in_favorites';
+  static const String inCart = 'in_cart';
 
   static const String data = 'data';
   static const String name = 'name';
   static const String image = 'image';
   static const String points = 'points';
   static const String credit = 'credit';
+  static const String banners = 'banners';
+  static const String products = 'products';
+  static const String category = 'category';
+  static const String product = 'product';
+  static const String images = 'images';
 
   static const String errors = 'errors';
   static const String badRequest = 'bad request';
