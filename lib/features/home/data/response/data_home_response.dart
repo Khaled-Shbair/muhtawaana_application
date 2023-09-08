cbd0a0fd546ba2e083fbaf2d88787d959e4e33d7
Khaled <khaled.shbair12@gmail.com>
2023-09-09 00:23:08 +0300

    create DataHomeResponse

---
+++ lib/features/home/data/response/data_home_response.dart
@@ -0,0 +1,24 @@
+import '/config/all_imports.dart';
+
+part 'data_home_response.g.dart';
+
+@JsonSerializable()
+class DataHomeResponse {
+  @JsonKey(name: ApiConstants.banners)
+  List<BannerDataHomeResponse>? banners;
+  @JsonKey(name: ApiConstants.products)
+  List<ProductDataHomeResponse>? products;
+
+  // @JsonKey(name: 'ad')
+  // String? ad;
+
+  DataHomeResponse({
+    this.banners,
+    this.products,
+  });
+
+  factory DataHomeResponse.fromJson(Map<String, dynamic> json) =>
+      _$DataHomeResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DataHomeResponseToJson(this);
+}
