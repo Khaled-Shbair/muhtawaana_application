fc3ce5e6a11559fd11abdd9b7795705d659955ab
Khaled <khaled.shbair12@gmail.com>
2023-09-09 12:01:19 +0300

    add new sizes

--- lib/core/resources/manager_sizes.dart
+++ lib/core/resources/manager_sizes.dart
@@ -1,13 +1,16 @@
 import '/config/all_imports.dart';
 
 class ManagerHeight {
+  static double h2 = SizeUtil.setHeight(2);
   static double h4 = SizeUtil.setHeight(4);
+  static double h7 = SizeUtil.setHeight(7);
   static double h10 = SizeUtil.setHeight(10);
   static double h12 = SizeUtil.setHeight(12);
   static double h14 = SizeUtil.setHeight(14);
   static double h16 = SizeUtil.setHeight(16);
   static double h20 = SizeUtil.setHeight(20);
   static double h24 = SizeUtil.setHeight(24);
+  static double h26 = SizeUtil.setHeight(26);
   static double h29 = SizeUtil.setHeight(29);
   static double h30 = SizeUtil.setHeight(30);
   static double h36 = SizeUtil.setHeight(36);
@@ -16,14 +19,18 @@ class ManagerHeight {
   static double h48 = SizeUtil.setHeight(48);
   static double h50 = SizeUtil.setHeight(50);
   static double h60 = SizeUtil.setHeight(60);
+  static double h66 = SizeUtil.setHeight(66);
   static double h70 = SizeUtil.setHeight(70);
   static double h76 = SizeUtil.setHeight(76);
   static double h77 = SizeUtil.setHeight(77);
   static double h80 = SizeUtil.setHeight(80);
+  static double h100 = SizeUtil.setHeight(100);
   static double h120 = SizeUtil.setHeight(120);
+  static double h125 = SizeUtil.setHeight(125);
   static double h200 = SizeUtil.setHeight(200);
   static double h202 = SizeUtil.setHeight(202);
   static double h205 = SizeUtil.setHeight(205);
+  static double h258 = SizeUtil.setHeight(258);
   static double h293 = SizeUtil.setHeight(293);
   static double h324 = SizeUtil.setHeight(324);
   static double h355 = SizeUtil.setHeight(355);
@@ -32,6 +39,7 @@ class ManagerHeight {
 class ManagerWidth {
   static double w84 = SizeUtil.setWidth(84);
   static double w2 = SizeUtil.setWidth(2);
+  static double w3 = SizeUtil.setWidth(3);
   static double w4 = SizeUtil.setWidth(4);
   static double w7 = SizeUtil.setWidth(7);
   static double w8 = SizeUtil.setWidth(8);
@@ -45,10 +53,14 @@ class ManagerWidth {
   static double w38 = SizeUtil.setWidth(38);
   static double w40 = SizeUtil.setHeight(40);
   static double w50 = SizeUtil.setHeight(50);
+  static double w65 = SizeUtil.setHeight(65);
   static double w74 = SizeUtil.setHeight(74);
+  static double w78 = SizeUtil.setHeight(78);
   static double w92 = SizeUtil.setHeight(92);
   static double w102 = SizeUtil.setWidth(102);
   static double w120 = SizeUtil.setWidth(120);
+  static double w148 = SizeUtil.setWidth(148);
+  static double w162 = SizeUtil.setWidth(162);
   static double w185 = SizeUtil.setWidth(185);
   static double w230 = SizeUtil.setWidth(230);
   static double w261 = SizeUtil.setWidth(261);
