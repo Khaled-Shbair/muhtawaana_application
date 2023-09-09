a72201186ea028028c10bcdd765aea4056686a5f
Khaled <khaled.shbair12@gmail.com>
2023-09-09 12:01:46 +0300

    add productDetailsScreen route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -7,4 +7,5 @@ class Routes {
   static const String mainScreen = '/main_screen';
   static const String forgetPasswordScreen = '/forget_password_screen';
   static const String changePasswordScreen = '/change_password_screen';
+  static const String productDetailsScreen = '/product_details_screen';
 }
