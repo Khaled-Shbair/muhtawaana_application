aa24c5f2a7dc9d50b91744755032023c5d978ce4
Khaled <khaled.shbair12@gmail.com>
2023-09-10 12:27:31 +0300

    create HomeScreen

---
+++ lib/features/home/presentation/view/screens/home_screen.dart
@@ -0,0 +1,47 @@
+import '/config/all_imports.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return willPopScope(
+      child: GetBuilder<HomeController>(
+        builder: (controller) {
+          return Scaffold(
+            backgroundColor: ManagerColors.whiteColor,
+            resizeToAvoidBottomInset: false,
+            appBar: homeAppBar(
+              imageUser: controller.imageUser,
+              nameUser: controller.nameUser,
+              onPressed: controller.buttonShopInAppBar,
+            ),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: ManagerHeight.h10),
+                    HomeSlider(sliders: controller.banners),
+                    TitleOfDepartment(
+                      name: ManagerStrings.sections,
+                      onTap: controller.buttonMoreSections,
+                    ),
+                    const HomeCategory(),
+                    TitleOfDepartment(
+                      name: ManagerStrings.products,
+                      onTap: controller.buttonMoreProducts,
+                    ),
+                    HomeProduct(
+                      products: controller.products,
+                      favoriteButton: controller.favoriteButton,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
