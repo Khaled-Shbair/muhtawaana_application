66c13d3254ab62b6631bdcb0e9ef246c2e39f7fa
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-10 17:24:25 +0300

    generate CategoriesResponse.g

---
+++ lib/features/categories/data/response/categories_response.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'categories_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CategoriesResponse _$CategoriesResponseFromJson(Map<String, dynamic> json) =>
+    CategoriesResponse(
+      status: json['status'] as bool?,
+      message: json['message'] as String?,
+      data: json['data'] == null
+          ? null
+          : CurrentPageOfCategoryResponse.fromJson(
+              json['data'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$CategoriesResponseToJson(CategoriesResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'data': instance.data,
+    };
