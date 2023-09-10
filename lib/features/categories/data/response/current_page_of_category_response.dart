1f7b081844b732a2236df08290b2485e84d062f2
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-10 17:24:40 +0300

    create CurrentPageOfCategoryResponse

---
+++ lib/features/categories/data/response/current_page_of_category_response.dart
@@ -0,0 +1,51 @@
+import '/config/all_imports.dart';
+
+part 'current_page_of_category_response.g.dart';
+
+@JsonSerializable()
+class CurrentPageOfCategoryResponse {
+  @JsonKey(name: ApiConstants.currentPage)
+  int? currentPage;
+  @JsonKey(name: ApiConstants.data)
+  List<DataCategoryResponse>? data;
+  @JsonKey(name: ApiConstants.firstPageUrl)
+  String? firstPageUrl;
+  @JsonKey(name: ApiConstants.from)
+  int? from;
+  @JsonKey(name: ApiConstants.to)
+  int? to;
+  @JsonKey(name: ApiConstants.lastPage)
+  int? lastPage;
+  @JsonKey(name: ApiConstants.perPage)
+  int? perPage;
+  @JsonKey(name: ApiConstants.total)
+  int? total;
+  @JsonKey(name: ApiConstants.lastPageUrl)
+  String? lastPageUrl;
+  @JsonKey(name: ApiConstants.path)
+  String? path;
+  @JsonKey(name: ApiConstants.nextPageUrl)
+  dynamic nextPageUrl;
+  @JsonKey(name: ApiConstants.prevPageUrl)
+  dynamic prevPageUrl;
+
+  CurrentPageOfCategoryResponse({
+    this.currentPage,
+    this.data,
+    this.perPage,
+    this.lastPage,
+    this.firstPageUrl,
+    this.to,
+    this.from,
+    this.path,
+    this.lastPageUrl,
+    this.nextPageUrl,
+    this.prevPageUrl,
+    this.total,
+  });
+
+  factory CurrentPageOfCategoryResponse.fromJson(Map<String, dynamic> json) =>
+      _$CurrentPageOfCategoryResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CurrentPageOfCategoryResponseToJson(this);
+}
---
+++ lib/features/categories/data/response/current_page_of_category_response.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'current_page_of_category_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CurrentPageOfCategoryResponse _$CurrentPageOfCategoryResponseFromJson(
+        Map<String, dynamic> json) =>
+    CurrentPageOfCategoryResponse(
+      currentPage: json['current_page'] as int?,
+      data: (json['data'] as List<dynamic>?)
+          ?.map((e) => DataCategoryResponse.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      perPage: json['per_page'] as int?,
+      lastPage: json['last_page'] as int?,
+      firstPageUrl: json['first_page_url'] as String?,
+      to: json['to'] as int?,
+      from: json['from'] as int?,
+      path: json['path'] as String?,
+      lastPageUrl: json['last_page_url'] as String?,
+      nextPageUrl: json['next_page_url'],
+      prevPageUrl: json['prev_page_url'],
+      total: json['total'] as int?,
+    );
+
+Map<String, dynamic> _$CurrentPageOfCategoryResponseToJson(
+        CurrentPageOfCategoryResponse instance) =>
+    <String, dynamic>{
+      'current_page': instance.currentPage,
+      'data': instance.data,
+      'first_page_url': instance.firstPageUrl,
+      'from': instance.from,
+      'to': instance.to,
+      'last_page': instance.lastPage,
+      'per_page': instance.perPage,
+      'total': instance.total,
+      'last_page_url': instance.lastPageUrl,
+      'path': instance.path,
+      'next_page_url': instance.nextPageUrl,
+      'prev_page_url': instance.prevPageUrl,
+    };
