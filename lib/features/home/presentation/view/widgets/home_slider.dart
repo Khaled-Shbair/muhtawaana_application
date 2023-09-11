67d2cb03a4b4380f6be9e59b98e564d6673f1c73
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-11 11:55:40 +0300

    refactor

--- lib/features/home/presentation/view/widgets/home_slider.dart
+++ lib/features/home/presentation/view/widgets/home_slider.dart
@@ -3,34 +3,44 @@ import '/config/all_imports.dart';
 class HomeSlider extends StatelessWidget {
   const HomeSlider({
     required this.sliders,
+    this.loading = false,
     super.key,
   });
 
   final List<BannerDataHomeModel> sliders;
+  final bool loading;
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      items: sliders.map((e) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(ManagerRadius.r10),
-          child: Image(
-            fit: BoxFit.fill,
-            image: NetworkImage(e.image),
-          ),
-        );
-      }).toList(),
-      options: CarouselOptions(
+    if (loading == false && sliders.isNotEmpty) {
+      return CarouselSlider(
+        items: sliders.map((e) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(ManagerRadius.r10),
+            child: Image(
+              fit: BoxFit.fill,
+              image: NetworkImage(e.image),
+              filterQuality: FilterQuality.high,
+            ),
+          );
+        }).toList(),
+        options: CarouselOptions(
+          height: ManagerHeight.h205,
+          initialPage: AppConstants.initialPageInSliderInHomeScreen,
+          enableInfiniteScroll: true,
+          reverse: false,
+          autoPlay: true,
+          enlargeCenterPage: true,
+          autoPlayCurve: Curves.fastOutSlowIn,
+          scrollDirection: Axis.horizontal,
+          viewportFraction: AppConstants.viewportFractionInSliderInHomeScreen,
+        ),
+      );
+    } else {
+      return MainShimmer(
         height: ManagerHeight.h205,
-        initialPage: AppConstants.initialPageInSliderInHomeScreen,
-        enableInfiniteScroll: true,
-        reverse: false,
-        autoPlay: true,
-        enlargeCenterPage: true,
-        autoPlayCurve: Curves.fastOutSlowIn,
-        scrollDirection: Axis.horizontal,
-        viewportFraction: AppConstants.viewportFractionInSliderInHomeScreen,
-      ),
-    );
+        width: ManagerWidth.w283,
+      );
+    }
   }
 }
