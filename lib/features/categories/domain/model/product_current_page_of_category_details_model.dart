ad4912d2b7671044a22c5078bac5d3d1e60f66bd
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-11 12:42:14 +0300

    create ProductCurrentPageOfCategoryDetailsModel

---
+++ lib/features/categories/domain/model/product_current_page_of_category_details_model.dart
@@ -0,0 +1,25 @@
+class ProductCurrentPageOfCategoryDetailsModel {
+  int id;
+  String name;
+  String image;
+  double price;
+  double oldPrice;
+  double discount;
+  String description;
+  List<String> images;
+  bool inFavorites;
+  bool inCart;
+
+  ProductCurrentPageOfCategoryDetailsModel({
+    required this.id,
+    required this.price,
+    required this.oldPrice,
+    required this.discount,
+    required this.inCart,
+    required this.inFavorites,
+    required this.description,
+    required this.name,
+    required this.image,
+    required this.images,
+  });
+}
