86dceed0ad87e8b91996bcc8c87b1962f0b52295
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-12 21:47:53 +0300

    create custom appBar to about app

--- lib/core/widgets/my_app_bar.dart
+++ lib/core/widgets/my_app_bar.dart
@@ -1,13 +1,13 @@
 import 'package:muhtawaana_app/config/all_imports.dart';
 
-AppBar categoriesAppBar(String text) {
+AppBar myAppBar(String text) {
   return AppBar(
     leading: const MyBackIcon(),
     backgroundColor: ManagerColors.whiteColor,
     elevation: AppConstants.elevationAppBar,
     title: Text(
       text,
-      style: titleAppBarInCategoriesScreen(),
+      style: titleMyAppBarWidget(),
     ),
   );
 }
