b489ed3bc7d47cde39d9ad8813b2493bdd63ac64
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-12 18:36:55 +0300

    add productDetails path api

--- lib/config/constants/requests_api.dart
+++ lib/config/constants/requests_api.dart
@@ -8,6 +8,6 @@ class RequestsApi {
   static const String logout = '${baseUrl}logout';
   static const String home = '${baseUrl}home';
   static const String categories = '${baseUrl}categories';
-  static const String category =
-      '${baseUrl}categories/{${ApiConstants.id}}';
+  static const String category = '${baseUrl}categories/{${ApiConstants.id}}';
+  static const String productDetails = '${baseUrl}products/{${ApiConstants.id}}';
 }
