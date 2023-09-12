2996af800be5f9ed1615b8c2473db71d55d86281
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-12 18:54:34 +0300

    create ProductDetailsModel

---
+++ lib/features/products/domain/model/product_details_model.dart
@@ -0,0 +1,13 @@
+import '/config/all_imports.dart';
+
+class ProductDetailsModel {
+  bool status;
+  String message;
+  DataOfProductDetailsModel data;
+
+  ProductDetailsModel({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+}
