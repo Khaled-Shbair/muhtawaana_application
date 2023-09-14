19ab9259c8c84fa2ab25cef8570dce71213349b8
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-14 23:18:01 +0300

    add new colors

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -23,6 +23,8 @@ class ManagerColors {
   static const Color c19 = Color(0XFFECECEC);
   static const Color c20 = Color(0XFF545454);
   static const Color c21 = Color(0XFF858383);
+  static const Color c22 = Color(0XFF888888);
+  static const Color c23 = Color(0XFF717171);
   static const Color shadow_09 = Color.fromRGBO(0, 0, 0, 0.09);
   static const Color shadow_15 = Color.fromRGBO(0, 0, 0, 0.15);
   static const Color shadow_1 = Color.fromRGBO(0, 0, 0, 0.1);
