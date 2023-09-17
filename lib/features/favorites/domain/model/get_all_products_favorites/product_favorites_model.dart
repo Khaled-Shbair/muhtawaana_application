f1337b4a98d040c1332a589f4a553b5d26809c04
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-17 18:44:23 +0300

    create ProductFavoritesModel

---
+++ lib/features/favorites/domain/model/get_all_products_favorites/product_favorites_model.dart
@@ -0,0 +1,11 @@
+import '/config/all_imports.dart';
+
+class ProductFavoritesModel {
+  int id;
+  DataOfProductFavoritesModel product;
+
+  ProductFavoritesModel({
+    required this.id,
+    required this.product,
+  });
+}
