82a6f7accbe87d0624eb184b4d7d1ef2e7583dcb
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-18 20:16:53 +0300

    refactor in ProductsOfCategoryScreen

--- lib/features/categories/presentation/view/screens/products_of_category_screen.dart
+++ lib/features/categories/presentation/view/screens/products_of_category_screen.dart
@@ -11,10 +11,43 @@ class ProductsOfCategoryScreen extends StatelessWidget {
       builder: (controller) {
         return Scaffold(
           appBar: myAppBar(text: ManagerStrings.categoryProducts),
-          body: StructureOfViewProduct(
-            favoriteButton: controller.favoriteButton,
-            loading: controller.loading,
-            products: controller.categoryDetails,
+          body: GridView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsetsDirectional.symmetric(
+              horizontal: ManagerWidth.w16,
+            ),
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: ManagerWidth.w10,
+              mainAxisSpacing: ManagerHeight.h14,
+              childAspectRatio: ManagerWidth.w162 / ManagerHeight.h258,
+            ),
+            itemCount: controller.loading == false
+                ? controller.categoryDetails.length
+                : 10,
+            itemBuilder: (context, index) {
+              if (controller.loading == true) {
+                return MainShimmer(
+                  height: ManagerHeight.h258,
+                  width: ManagerWidth.w162,
+                );
+              } else {
+                var data = controller.categoryDetails[index];
+                return StructureOfViewProduct(
+                  image: data.image,
+                  discount: data.discount,
+                  id: data.id,
+                  inFavorites: data.inFavorites,
+                  name: data.name,
+                  oldPrice: data.oldPrice,
+                  price: data.price,
+                  data: data,
+                  buttonFavorites: () {},
+                  // buttonFavorites: () => controller.buttonFavorites(data.id),
+                );
+              }
+            },
           ),
         );
       },
