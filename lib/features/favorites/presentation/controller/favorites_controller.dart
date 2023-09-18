379fe0c08ef472da8cd8fb4991a19e0e5d1afd92
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-18 20:00:40 +0300

    complete FavoritesController

--- lib/features/favorites/presentation/controller/favorites_controller.dart
+++ lib/features/favorites/presentation/controller/favorites_controller.dart
@@ -3,10 +3,8 @@ import '/config/all_imports.dart';
 class FavoritesController extends GetxController with ShowSnackBar {
   static FavoritesController get to => Get.find();
   final FavoritesUseCase _useCase = instance<FavoritesUseCase>();
-  final AllProductsFavoritesUseCase _allProductsFavoritesUseCase =
-      instance<AllProductsFavoritesUseCase>();
-  List<ProductFavoritesModel> productsFavorites = [];
   bool loading = false;
+  List<ProductDataHomeModel> productsFavorites = [];
 
   @override
   onInit() {
@@ -17,27 +15,27 @@ class FavoritesController extends GetxController with ShowSnackBar {
   Future<void> addOrDeleteProductFavorites(int productId) async {
     (await _useCase.execute(FavoritesBaseUseCaseInput(productId: productId)))
         .fold(
-      (l) {
-        showSnackBar(l.message, true);
-      },
-      (r) async {
-        showSnackBar(r.message);
-        await getAllProductsFavorites();
-      },
+      (l) => showSnackBar(l.message, true),
+      (r) => getAllProductsFavorites(),
     );
     update();
   }
 
-  Future<void> getAllProductsFavorites() async {
-    loading = true;
-    productsFavorites = [];
-    (await _allProductsFavoritesUseCase.execute()).fold(
-      (l) {},
-      (r) {
-        productsFavorites = r.data.data;
-        loading = false;
-      },
-    );
+  void buttonFavorites(int productId) async {
+    HomeController.to.buttonFavorites(productId);
+    getAllProductsFavorites();
     update();
+  }
+
+  Future<void> getAllProductsFavorites() async {
+    productsFavorites = [];
+    loading = true;
+    for (var e in HomeController.to.products) {
+      if (e.inFavorites) {
+        productsFavorites.add(e);
+      }
+    }
+    update();
+    loading = false;
   }
 }
