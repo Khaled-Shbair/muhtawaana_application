20163f0c00ff1afd7d9bb44f727e7e26c815bb02
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-20 13:24:17 +0300

    change to DataOfEditProfileRequest

---
+++ lib/features/profile/data/request/data_of_edit_profile_request.dart
@@ -0,0 +1,13 @@
+class DataOfEditProfileRequest {
+  String name;
+  String email;
+  String phone;
+  String image;
+
+  DataOfEditProfileRequest({
+    required this.name,
+    required this.image,
+    required this.phone,
+    required this.email,
+  });
+}
