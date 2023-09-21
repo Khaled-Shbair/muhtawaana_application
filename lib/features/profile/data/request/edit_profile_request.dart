550fab79107c3ecf8db81c6a5d2d5f54820057db
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-21 11:53:30 +0300

    refactor class

---
+++ lib/features/profile/data/request/edit_profile_request.dart
@@ -0,0 +1,13 @@
+class EditProfileRequest {
+  String? name;
+  String? email;
+  String? phone;
+  String? image;
+
+  EditProfileRequest({
+    this.name,
+    this.image,
+    this.phone,
+    this.email,
+  });
+}
