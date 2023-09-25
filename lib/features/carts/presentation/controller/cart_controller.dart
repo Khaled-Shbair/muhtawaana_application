9fa40e24ffdf5fee7833349f8cb4f3fe7658f219
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-25 23:32:30 +0300

    add getAllCartProducts function to get and save cart products

--- lib/features/carts/presentation/controller/cart_controller.dart
+++ lib/features/carts/presentation/controller/cart_controller.dart
@@ -2,12 +2,16 @@ import '/config/all_imports.dart';
 
 class CartController extends GetxController with ShowSnackBar {
   static CartController get to => Get.find();
-  final AddOrDeleteProductCartUseCase _useCase =
+  final AddOrDeleteProductCartUseCase _addOrDeleteProductCartUseCase =
       instance<AddOrDeleteProductCartUseCase>();
+  final GetAllCartProductsUseCase _getAllCartProductsUseCase =
+      instance<GetAllCartProductsUseCase>();
   bool loading = false;
+  List allCartProducts = [];
+  double totalPrice = 0;
 
   Future<void> addOrDeleteProductCart(int productId) async {
-    (await _useCase.execute(
+    (await _addOrDeleteProductCartUseCase.execute(
       AddOrDeleteProductCartBaseUseCaseInput(productId: productId),
     ))
         .fold(
@@ -16,6 +20,23 @@ class CartController extends GetxController with ShowSnackBar {
       },
       (r) {},
     );
+    update();
+  }
+
+  Future<void> getAllCartProducts() async {
+    allCartProducts = [];
+    loading = true;
+    (await _getAllCartProductsUseCase.execute()).fold(
+      (l) {
+        showSnackBar(l.message, true);
+      },
+      (r) {
+        allCartProducts = r.data.cartItems;
+        totalPrice = r.data.total;
+        loading = false;
+      },
+    );
+
     update();
   }
 }
