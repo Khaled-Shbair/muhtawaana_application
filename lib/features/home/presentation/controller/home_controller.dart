57a561c8cf745a56b17502326daac34acb7bf842
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-27 01:48:17 +0300

    edit in home_controller.dart

--- lib/features/home/presentation/controller/home_controller.dart
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -34,6 +34,8 @@ class HomeController extends GetxController with ShowSnackBar {
     loading = true;
     (await _useCase.execute()).fold(
       (l) {
+        getHomeData();
+        CartController.to.getAllCartProducts();
         showSnackBar(l.message, true);
       },
       (r) {
@@ -86,7 +88,10 @@ class HomeController extends GetxController with ShowSnackBar {
     await Get.toNamed(Routes.productDetailsScreen, arguments: id);
   }
 
-  void addToCart() {}
+  void addToCart(int productId) async {
+    await CartController.to.addOrDeleteProductCart(productId, true);
+    await CartController.to.getAllCartProducts();
+  }
 
   void buttonFavorites(int productId) {
     for (var e in products) {
