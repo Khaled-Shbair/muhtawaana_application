934ab514fb544cf4aff4e977b5ef83d764879358
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-26 17:45:15 +0300

    add path of new images

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -31,9 +31,18 @@ class ManagerAssets {
 
   //Icons images
   static const String arrowBackIcon = '${_imagePath}icon/arrow_back_icon.svg';
+  static const String deleteIcon = '${_imagePath}icon/delete_icon.svg';
   static const String searchIcon = '${_imagePath}icon/search_icon.svg';
+  static const String plusIcon = '${_imagePath}icon/plus_icon.svg';
+  static const String minimizeIcon = '${_imagePath}icon/minimize_icon.svg';
+
   static const String nullImageCategory =
       'https://student.valuxapps.com/storage/uploads/categories/16893929290QVM1.modern-devices-isometric-icons-collection-with-sixteen-isolated-images-computers-periphereals-variou.jpeg';
+
+//Empty images
+  static const String cartEmpty = '${_imagePath}empty/cart_empty.svg';
+  static const String notificationEmpty =
+      '${_imagePath}empty/notification_empty.svg';
 }
 
 class ManagerJson {}
