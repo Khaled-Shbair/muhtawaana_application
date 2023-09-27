4cade48feb92b6440adf4c7effa62c99cfc4b1d1
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-28 01:52:39 +0300

    some edit in native code

--- android/app/src/main/kotlin/muhtawaana/com/muhtawaana_application/MainActivity.kt
+++ android/app/src/main/kotlin/muhtawaana/com/muhtawaana_application/MainActivity.kt
@@ -1,6 +1,7 @@
 package muhtawaana.com.muhtawaana_application
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
