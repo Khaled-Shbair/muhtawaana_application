9d389310dbf27cb15380cb58bf65dd28b23bbdaa
Khaled Shbair <khaled.shbair12@gmail.com>
2023-09-28 18:29:54 +0300

    remove http and use dio

--- lib/features/payment/presentation/controller/payment_controller.dart
+++ lib/features/payment/presentation/controller/payment_controller.dart
@@ -1,48 +1,77 @@
 import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import '/config/all_imports.dart';
-import 'package:http/http.dart' as http;
 
 class PaymentController extends GetxController with ShowSnackBar {
   static PaymentController get to => Get.find();
+  String _clientSecret = '';
 
-  /// Stripe accept only pay by (Cents).  >>>>>  {1 dolors = 100 Cents}
+  Future<void> _getClientSecret(String amount, String currency) async {
+    try {
+      Stripe.publishableKey = ApiConstants.publishableKey;
 
-  Future<String> _makePaymentIntent(String amount, String currency) async {
-    Stripe.publishableKey = ApiConstants.publishableKey;
-    final response = await http.post(
-      Uri.parse(RequestsApi.paymentUrl),
-      headers: {
-        HttpHeaders.authorizationHeader:
-            '${ApiConstants.bearer} ${ApiConstants.secretKey}',
-      },
-      body: {
-        ApiConstants.amount: amount,
-        ApiConstants.currency: currency,
-      },
-    );
-
-    return jsonDecode(response.body)[ApiConstants.clientSecret];
+      Dio dio = Dio();
+      final response = await dio.post(
+        RequestsApi.paymentUrl,
+        options: Options(
+          headers: {
+            HttpHeaders.authorizationHeader:
+                '${ApiConstants.bearer} ${ApiConstants.secretKey}',
+            Headers.contentTypeHeader: Headers.formUrlEncodedContentType,
+          },
+        ),
+        data: {
+          ApiConstants.amount: amount,
+          ApiConstants.currency: currency,
+        },
+      );
+      _clientSecret = response.data[ApiConstants.clientSecret];
+    } catch (e) {
+      showSnackBar(ManagerStrings.somethingWentWrongTryAgain, true);
+    }
   }
 
   Future<void> _initPaymentSheet(String clientSecret) async {
-    await Stripe.instance.initPaymentSheet(
+    await Stripe.instance
+        .initPaymentSheet(
       paymentSheetParameters: SetupPaymentSheetParameters(
         paymentIntentClientSecret: clientSecret,
         merchantDisplayName: 'Payment',
         allowsDelayedPaymentMethods: true,
       ),
-    );
+    )
+        .then((value) {
+      showDialog(
+        context: Get.context!,
+        builder: (context) {
+          return const AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.check_circle,
+                      color: ManagerColors.successColor,
+                    ),
+                    Text('Payment Successfully'),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    });
   }
 
   Future<void> makePayment(double amount) async {
-    try {
-      String price = (amount * 100).toInt().toString();
-      String clientSecret = await _makePaymentIntent(price, 'USD');
-      await _initPaymentSheet(clientSecret);
+    String price = (amount * 100).toInt().toString();
+    await _getClientSecret(price, 'USD');
+    if (_clientSecret.isNotEmpty) {
+      await _initPaymentSheet(_clientSecret);
       await Stripe.instance.presentPaymentSheet();
-    } catch (e) {
-      // showSnackBar(ManagerStrings.somethingWentWrongTryAgain, true);
     }
   }
 }
