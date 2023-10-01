8dd6060fb9e24c5c6522a1954687b7a41806429c
Khaled Shbair <khaled.shbair12@gmail.com>
2023-10-02 02:29:24 +0300

    create DataOfProfileModel

---
+++ lib/features/profile/domain/model/data_of_profile_model.dart
@@ -0,0 +1,21 @@
+class DataOfProfileModel {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String image;
+  String token;
+  int points;
+  int credit;
+
+  DataOfProfileModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.image,
+    required this.token,
+    required this.credit,
+    required this.points,
+  });
+}
