75d2895dbc5a3001fd1fb2388039e411a749c2e7
Khaled Shbair <khaled.shbair12@gmail.com>
2023-10-02 02:29:31 +0300

    create ProfileModel

---
+++ lib/features/profile/domain/model/profile_model.dart
@@ -0,0 +1,13 @@
+import '/config/all_imports.dart';
+
+class ProfileModel {
+  bool status;
+  String message;
+  DataOfProfileModel data;
+
+  ProfileModel({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+}
