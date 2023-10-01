35abb622a75dd0b6f1d2c1b287df5030b51e6b13
Khaled Shbair <khaled.shbair12@gmail.com>
2023-10-02 02:43:32 +0300

    create ProfileMapper

---
+++ lib/features/profile/data/mapper/profile_mapper.dart
@@ -0,0 +1,11 @@
+import '/config/all_imports.dart';
+
+extension ProfileMapper on ProfileResponse {
+  toDomain() {
+    return ProfileModel(
+      status: status.onNull(),
+      message: message.onNull(),
+      data: data?.toDomain(),
+    );
+  }
+}
